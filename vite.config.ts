ae26424e3a51fbea9c81d9e3291ebe0619088a00
Jhones Sandro <jhou05@gmail.com>
2026-09-15 08:49:46 -0300

    feat: migrar hospedagem para firebase hosting

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
